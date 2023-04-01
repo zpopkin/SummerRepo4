5a6659b76f020cd7d3d350fed492b8a48feff090
Zach <117704796+zpopkin@users.noreply.github.com>
2023-04-01 11:47:58 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,15 +2,14 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is Pete Magee
+# My name is Zach Popkin
 # This is my coding that generates Boxplots for transmission types
 # The data source is the mpg data table
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
   xlab("transmission types") +
-  ylab("displacement") +
-coord_flip()
+  ylab("displacement")
   
 
 
